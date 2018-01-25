eb219d4c0a31fa4e92f989f908718388534cfd55
Alicia Florez <alicia.florez@abc-culture.fr>
2018-01-25 11:25:03 +0100

    Add some tests with tox and pytest

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+tests_require = [
+    'pytest',
+    'pytest-pep8',
+    'pytest-cov',
+]
+
+setup(
+    name='pydelivengo',
+    keywords=['api', 'mydelivengo', 'python', 'webservices'],
+    license='MIT',
+)
