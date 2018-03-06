b8875bcd7d4dc997ffff24945ee137d45965736e
Alicia Florez <alicia.florez@abc-culture.fr>
2018-03-06 15:54:40 +0100

    Hotfix

--- pydelivengo/__init__.py
+++ pydelivengo/__init__.py
@@ -1,1 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2018 Alexandria
+#
+# Distributed under terms of the MIT license.
+
+from __future__ import absolute_import
+
 from pydelivengo.pydelivengo import PyDelivengo
