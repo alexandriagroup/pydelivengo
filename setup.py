1757d0cd959231cfd207827e2e6623e0ee0a58ec
Alicia Florez <alicflorez@users.noreply.github.com>
2018-10-22 16:21:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 #
 # Distributed under terms of the MIT license.
 
-from setuptools import setup
+import setuptools
 
 tests_require = [
     'pytest',
@@ -12,15 +12,31 @@ tests_require = [
     'pytest-cov',
 ]
 
-setup(
-    name='pydelivengo',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydelivengo",
+    version="v1.3",
     license='MIT',
-    version = 'v1.3',
-    description = 'A lib to use MyDelivengo API',
-    author = 'Alicia FLOREZ',
-    author_email = 'alicflorez@gmail.com',
-    url = 'https://github.com/alexandriagroup/pydelivengo',
-    download_url = 'https://github.com/alexandriagroup/pydelivengo/archive/v1.3.tar.gz',
-    keywords=['api', 'mydelivengo', 'python', 'webservices', 'la poste'],
+    author="Alicia FLOREZ",
+    author_email="alicflorez@gmail.com",
+    description="A lib to use MyDelivengo API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexandriagroup/pydelivengo",
+    project_urls = {
+        'Source': 'https://github.com/alexandriagroup/pydelivengo/archive/v1.3.tar.gz',
+        'Documentation': 'https://alexandriagroup.github.io/pydelivengo/',
+        'Travis': 'https://travis-ci.org/alexandriagroup/pydelivengo',
+    },
     packages=['pydelivengo'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Development Status :: 5 - Production/Stable',
+    ],
+    keywords='api mydelivengo python3 webservices',
+
 )
